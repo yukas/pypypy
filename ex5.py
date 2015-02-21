fe1995657795f6295a4d37a2399559b2d5554324
Yuri Kaspervich <ykas.gg@gmail.com>
2015-02-21 14:10:21 +0300

    Change names in ex5

--- ex5.py
+++ ex5.py
@@ -1,19 +1,19 @@
 # -- coding: utf-8 --
-my_name = 'Zed A.Shaw'
-my_age = 35 # not a liea
-my_height = 74
-my_weight = 180
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A.Shaw'
+age = 35 # not a liea
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "Let's talk about %r." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee" % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee" % teeth
 
 # this line is tricky, try to get it exactly right
-print "If add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age
-    + my_height + my_weight)
+print "If add %d, %d, and %d I get %d." % (age, height, weight, age
+    + height + weight)
