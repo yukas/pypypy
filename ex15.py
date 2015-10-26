2ca34ff7025e4b851e444c1e38a57f2a68c855b1
Yuri Kaspervich <ykas.gg@gmail.com>
2015-10-26 12:44:53 +0300

    Study drills for exercise 15

--- ex15.py
+++ ex15.py
@@ -1,15 +1,27 @@
+# Imports argv module
 from sys import argv
 
+# Unpacks what is in argv variable into two other variables
 script, filename = argv
 
+# Opens file
 txt = open(filename)
 
+# Prints the name of opened file
 print "Here's your file %r:" % filename
+# Reads contents of the file and prints it
 print txt.read()
 
+# Prints a string
 print "Type the filename again:"
+# Reads other file name from user input
 file_again = raw_input("> ")
 
+# Opens another fiel
 txt_again = open(file_again)
 
+# Reads file
 print txt_again.read()
+
+txt.close()
+txt_again.close()
