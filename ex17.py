786bae793949bebb75a48da8d65b60c249ab3ea4
Yuri Kaspervich <ykas.gg@gmail.com>
2015-10-27 23:56:55 +0300

    Modify exercise 17 according to study drills

--- ex17.py
+++ ex17.py
@@ -3,21 +3,18 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print "Copying from %s to %s" % (from_file, to_file)
-
 in_file = open(from_file)
 indata = in_file.read()
 
-print "The input file is %d bytes long" % len(indata)
+print "Copying %d bytes from %s to %s..." % (len(indata), from_file, to_file)
 
-print "Does the output file exist? %r" % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
-
-out_file = open(to_file, 'w')
+out_file = open(to_file, 'w+')
 out_file.write(indata)
 
 print "Alright, all done."
 
-out_file.close()
+print "Content of in_file: %r" %  in_file.read()
+print "Content of out_file: %r" % out_file.read()
+
 in_file.close()
+out_file.close()
